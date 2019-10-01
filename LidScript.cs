3251601bf0b5bead5273bd47ea09ebeab0406830
VirtualVortex <mcoates111@gmail.com>
2019-10-01 20:08:40 +0100

    Create LidScript.cs

---
+++ LidScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LidScript : MonoBehaviour {
+
+    [SerializeField]
+    private Arduino arduino;
+
+    private Vector3 currentPos;
+
+    public bool lidIsOpen;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+        if (arduino != null)
+        {
+            //when e is detected the lid will appear else it will disapear
+            if (arduino.val == "e")
+            {
+                lidIsOpen = false;
+                GetComponent<MeshRenderer>().enabled = true;
+            }
+            else
+            {
+                lidIsOpen = true;
+                GetComponent<MeshRenderer>().enabled = false;
+            }
+        }
+
+        
+    }
+}
