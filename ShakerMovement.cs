871dd94889c22b64a8df66d16b031cd0ced245a9
VirtualVortex <mcoates111@gmail.com>
2019-10-01 20:08:27 +0100

    Create ShakerMovement.cs

---
+++ ShakerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Events;
+
+public class ShakerMovement : MonoBehaviour {
+
+    [SerializeField]
+    private bool forTesting;
+
+    private bool isOpen;
+    private Transform t;
+
+    public UnityEvent shaker;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //If forTesting is true then the player will control the shaker via the keyboard else it will invoke the registerted function
+        if (forTesting)
+            transform.Translate(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        else
+            shaker.Invoke();
+	}
+}
