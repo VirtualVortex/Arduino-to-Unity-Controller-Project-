67cdf238ed12a6bb016a62a2de6efeedc9f2231a
VirtualVortex <mcoates111@gmail.com>
2019-10-01 20:08:35 +0100

    Create Score.cs

---
+++ Score.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    private float score;
+
+    public Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+        //At the start the player score will be displayed
+        scoreText.text = PlayerPrefs.GetFloat("Score", score).ToString();
+	}
+}
