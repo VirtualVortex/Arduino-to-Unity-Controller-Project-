897286abd26ed1f24d9d536f2827ac4486c18be6
VirtualVortex <mcoates111@gmail.com>
2019-10-01 20:08:51 +0100

    Create ChangeScene.cs

---
+++ ChangeScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+    [SerializeField]
+    private string scene;
+
+    //The method below will chnge the scene depending on what's been set
+    public void SwitchtoScene()
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
