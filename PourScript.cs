d3d057c58d4a318012c246f2f5ccaf86afdfc8df
VirtualVortex <mcoates111@gmail.com>
2019-10-01 20:08:38 +0100

    Create PourScript.cs

---
+++ PourScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PourScript : MonoBehaviour {
+
+    [SerializeField]
+    private Transform spawnPoint;
+    [SerializeField]
+    private GameObject drink;
+    [SerializeField]
+    private Shakermeter sm;
+    [SerializeField]
+    private Arduino arduino;
+
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Debug.Log("Lid is open: " + lidIsOpen);
+
+        Debug.Log("is shaken: " + sm.isShaken);
+
+        if (sm != null )
+        {
+            //If the drink is shaken and is tilted updside down then the drink will be spawned
+            if (sm.isShaken)
+            {
+                if (transform.eulerAngles.z < 220 && transform.eulerAngles.z > 150)
+                {
+                    Instantiate(drink, spawnPoint.position, Quaternion.identity);
+                }
+            }
+            
+        }
+
+        
+    }
+}
