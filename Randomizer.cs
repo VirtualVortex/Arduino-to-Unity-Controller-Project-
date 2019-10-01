ec715e6c47e3a4feefc7bf148e08d351aeb53057
VirtualVortex <mcoates111@gmail.com>
2019-10-01 20:08:37 +0100

    Create Randomizer.cs

---
+++ Randomizer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Randomizer : MonoBehaviour {
+    
+    [SerializeField]
+    private Image Layer;
+    [SerializeField]
+    private Transform layerBackground;
+    [SerializeField]
+    public List<Image> Layers = new List<Image>();
+    public List<int> layerNums = new List<int>();
+
+
+    string[] ingrediantsList = new string[] { "Sugar Syrup", "Lemon Juice", "Lime Juice", "Dry Gin", "Vodka"};
+    int randomNum;
+
+    // Use this for initialization
+    void Start () {
+        //Create list at beggining
+        RandomLayers();
+	}
+
+    public void RandomLayers()
+    {
+        //For every layer it will be given a value which is diasplayed on the UI list and added a list for the check script
+        foreach (Image layer in Layers)
+        {
+            //string String = ingrediantsList[0].ToString();
+            randomNum = Random.Range(0, ingrediantsList.Length);
+
+            layer.GetComponentInChildren<Text>().text = ingrediantsList[randomNum];
+            layerNums.Add(randomNum);
+        }
+    }
+
+    //When called the previous list will be removed
+    public void RemoveOldList()
+    {
+        layerNums.Clear();
+    }
+}
