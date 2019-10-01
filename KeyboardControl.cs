489fdc73bb7df9daeba840c92de3fce3c4ae6fc4
VirtualVortex <mcoates111@gmail.com>
2019-10-01 20:08:42 +0100

    Create KeyboardControl.cs

---
+++ KeyboardControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Control the movement of the shaker with keyboard instead of controller
+        transform.position = new Vector3(Input.GetAxis("Horizontal"), 0 ,Input.GetAxis("Vertical"));
+	}
+}
